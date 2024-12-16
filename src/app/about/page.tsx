d30353cab645d886bcb138cd6e8d745b99969bd0
Ahmedkhang <160028006+Ahmedkhang@users.noreply.github.com>
2024-12-16 13:07:29 -0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,7 +23,7 @@ export default function Aboutus() {
                 />
                 <div className='w-full text-white absolute text-center top-40 left-0 items-center'>
                     <h1 className='text-5xl font-bold'>About Us</h1>
-                    <h5 className='py-3'>Home &gt; <span className='text-[#FF9F0D]'>About</span></h5>
+                    <h5 className='py-3'>Home &gt; About</h5>
                 </div>
             </div>
 
