c6885b4a3f5103c2f3f696e3cd0c88a8dc038edf
Ahmedkhang <160028006+Ahmedkhang@users.noreply.github.com>
2024-12-16 13:06:29 -0800

    Update page.tsx

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -47,7 +47,7 @@ const FAQPage: React.FC = () => {
                 />
                 <div className='w-full text-white absolute text-center top-40 left-0 items-center'>
                     <h1 className='text-5xl font-bold'>FAQ</h1>
-                    <h5 className='py-3'>Home > <span className='text-[#FF9F0D]'>FAQ</span></h5>
+                    <h5 className='py-3'>Home &gt; <span className='text-[#FF9F0D]'>FAQ</span></h5>
                 </div>
             </div>
 
