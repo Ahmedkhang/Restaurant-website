576f076c659ec4ba1f9be4b7c985337d10738a58
Ahmedkhang <160028006+Ahmedkhang@users.noreply.github.com>
2024-12-16 13:05:02 -0800

    Update page.tsx

--- src/app/404Error/page.tsx
+++ src/app/404Error/page.tsx
@@ -17,7 +17,7 @@ export default function ErrorPage() {
                 />
                 <div className='w-full text-white absolute text-center top-40 left-0 items-center'>
                     <h1 className='text-5xl font-bold'>Error 404</h1>
-                    <h5 className='py-3'>Home > <span className='text-[#FF9F0D]'>Error 404</span></h5>
+                    <h5 className='py-3'>Home &gt;<span className='text-[#FF9F0D]'>Error 404</span></h5>
                 </div>
             </div>
     <div className="min-h-screen bg-white">
